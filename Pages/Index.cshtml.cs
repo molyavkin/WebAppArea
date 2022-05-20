2b41dc475f51c778e6fb5fc587c07dffd07b4fde
molyavkin <molyavkin@mail.ru>
2022-05-20 15:45:12 +0500

    Complete

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using CalculatingAreaLib;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppArea.Pages
+{
+    [IgnoreAntiforgeryToken]
+    public class IndexModel : PageModel
+    {
+        public string Message { get; set; } = "";
+        public void OnGet()
+        {
+
+        }
+        public void OnPost(double radius)
+        {
+            if (radius < 0)
+            {
+                Message = "Некорректный ввод";
+            }
+            else
+            {
+                Circle circle = new Circle(radius);
+                double areaCircle = circle.CalculateArea();
+                Message = $"Площадь круга = {areaCircle:#,##0.00}";
+                
+            }
+
+        }
+    }
+}
---
+++ Pages/Triangle.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using CalculatingAreaLib;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppArea.Pages
+{
+    [IgnoreAntiforgeryToken]
+    public class TriangleModel : PageModel
+    {
+        public string Message { get; set; } = "";
+        public void OnGet()
+        {
+            
+        }
+        public void OnPost(double a, double b, double c)
+        {
+            Triangle triangle = new Triangle(a, b, c);  
+            if (!triangle.IsExistShape())
+            {
+                Message = "Треугольник с такими сторонами не существует";
+            }
+            else
+            {
+                double areaTriangle = triangle.CalculateArea();
+                if (triangle.IsRightAngled())
+                {
+                    Message = "Треугольник прямоугольный. ";
+                }
+                Message += $"Площадь треугольника = {areaTriangle:#,##0.00}";
+            }
+
+        }
+    }
+}
